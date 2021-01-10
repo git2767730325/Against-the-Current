3795d2a67db963b661722717b9b91f46c2e73159
git2767730325 <2767730325@qq.com>
2021-01-10 20:26:40 +0800

    first

---
+++ Against the Current/Assets/Scirpts/ActorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour
+{
+    public GameObject model;
+    public PlayerHandle playH;
+    public float walkSpeed;
+    [SerializeField]
+    private Animator anim;
+    private Rigidbody rig;
+    private Vector3 moveVec;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        playH = GetComponent<PlayerHandle>();
+        anim = model.GetComponent<Animator>();
+        rig = gameObject.GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playH.Dmo > 0.01f) 
+        anim.transform.forward = playH.Dvec;
+        anim.SetFloat("forward", playH.Dmo);
+        moveVec = playH.Dmo * playH.Dvec*walkSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rig.velocity = new Vector3(moveVec.x,rig.velocity.y,moveVec.z);//y=0,避免重力失效
+    }
+}
---
+++ Against the Current/Assets/Scirpts/PlayerHandle.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandle : MonoBehaviour
+{
+    public string up;
+    public string down;
+    public string left;
+    public string right;
+
+    [Header("Signal")]
+    public float Dup;
+    public float Dright;
+
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+
+    [Header("Signal2")]
+    public float Dmo;
+    public Vector3 Dvec;
+
+    [Header("Bool")]
+    public bool inputEnable = true;
+
+
+
+
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //目标速度值
+        targetDup = (Input.GetKey(up) ? 1.0f : 0f) + (Input.GetKey(down) ? -1.0f : 0f);//记得括号，3元运算先后顺序不同
+        targetDright = (Input.GetKey(right) ? 1.0f : 0f) + (Input.GetKey(left) ? -1.0f : 0f);
+        //软控制，避免数值大乱
+        if(!inputEnable)
+        {
+            targetDright = targetDup = 0.0f;//清空输入
+        }
+
+        //使用平滑达到目标速度，时间0.2s可变
+        Dup = Mathf.SmoothDamp(Dup, targetDup,ref velocityDup, 0.2f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.2f);
+        //方向和速度向量需要纠正
+        Dmo = Mathf.Sqrt(Dup*Dup + Dright*Dright);
+        Dvec = Dup * Vector3.forward + Dright * Vector3.right;
+        //
+    }
+}
